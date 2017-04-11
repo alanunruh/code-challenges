1a3c55586671fb1a17f8637640f11b54f8cdc95f
Alan Unruh <alan.unruh@gmail.com>
2017-04-11 22:24:37 +0000

    refactor

--- card-deck.rb
+++ card-deck.rb
@@ -9,71 +9,63 @@ class Card
   def output_card
     puts "#{self.rank} of #{self.suit}"
   end
-	
+
   def self.random_card
-    Card.new(rand(10), :spades)
+    Card.new(rand(13), :spades)
   end
 end
 
 class Deck
-	
-	def initialize
-		# Builds a deck of cards
-		@deck_of_cards = []
-			ranks = [:Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, :Jack, :Queen, :King]
-			suits = [:Hearts, :Diamonds, :Spade, :Clubs]
 
-		# Loops through and pairs rank with suit creating a deck
-		ranks.each do |rank|
-			suits.each do |suit|
-				@deck_of_cards << Card.new(rank, suit)
-			end
-		end
-	end
+  def initialize
+    @cards = []
+    @ranks = [:Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, :Jack, :Queen, :King]
+    @suits = [:Hearts, :Diamonds, :Spades, :Clubs]
 
-	# Defines a rank
-	def rank
-		@ranks
-	end
+    @ranks.each do |rank|
+      @suits.each do |suit|
+        @cards << Card.new(rank, suit)
+      end
+    end
+  end
 
-	# Defines a suit
-	def suit
-		@suits
-	end
+  def rank
+    @ranks
+    puts "Here are the rank of the cards"
+  end
 
-	#Defines a deck of cards
-	def deck_of_cards
-		@deck_of_cards
-	end
+  def suits
+    @suits
+    puts "Here are the suits of the cards"
+  end
 
-	def shuffle
-		@deck_of_cards.shuffle!
-	end
+  def cards
+    @cards
+    puts "Here is the deck of cards"
+  end
 
-	def deal
-		top_card = @deck_of_cards.shift
-			puts "#{top_card.rank} of #{top_card.suit}"
-	end
+  def output
+    @cards.each do |card|
+      card.output_card
+    end
+    return "Here's your deck of cards"
+  end
 
-	def ouput	
-		@deck_of_cards.each do |card|
-			card.output_card
-		end
-	end
-end	
+  def shuffle
+    @cards.shuffle!
+  end
+
+  def deal
+    top_card = @cards.shift
+    top_card.output_card
+  end
+
+  def shuffle_and_deal
+    self.shuffle
+    self.deal
+  end
+end
 
 deck = Deck.new
-#Show deck number
-#puts deck
-#Show deck rank
-#puts deck.rank
-#Show deck suit
-#puts deck.suit
-#Show deck of cards => Does not ouput blended @ranks & @suits
-#puts deck.deck_of_cards
-#Shuffles and shows the deck of cards
-#puts deck.shuffle
-#Deals the top card off the deck => Outputs actual ranks and suit
-#puts deck.deal
-#Shuffles the deck and outputs the top card => Not working
-#puts deck.shuffle.deal
+
+puts deck.shuffle_and_deal
