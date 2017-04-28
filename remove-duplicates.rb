550e8d3560c7d9b3211e966cd1b0ac42e78b2c78
Alan Unruh <alan.unruh@gmail.com>
2017-04-28 13:01:47 +0000

    Remove duplicates

---
+++ remove-duplicates.rb
@@ -0,0 +1,15 @@
+# Define the unique method that removes duplicates
+def unique(integers)
+  @trash = []
+  @keep = []
+  duplicates = {}
+  
+    integers.each do |i|
+      if duplicates [i]
+        @trash << i
+      else
+        @keep << i
+      end
+    end
+
+end
