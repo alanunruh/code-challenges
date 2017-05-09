575494ef23de1e2bbfbd54d19262603d50fa9ec1
Alan Unruh <alan.unruh@gmail.com>
2017-05-09 00:30:14 +0000

    Image blur #2 working

--- image-blur-2.rb
+++ image-blur-2.rb
@@ -1,43 +1,80 @@
+require 'pry'
 class Image
+
+  attr_reader :one_pixel_indices
+
   def initialize(data)
     @rows = data
+    find_one_coordinates
   end
 
   def output_image
     @rows.each{|row| puts row.join}
   end
 
-  def one_coordinates
-    one_pixel_indices = []
+  def find_one_coordinates
+    @one_pixel_indices = []
     @rows.each_with_index do |row, row_index|
       row.each_with_index do |column, column_index|
+        # binding.pry if column == 1
         if column == 1
-          one_pixel_indices << [row_index, column_index]
+          @one_pixel_indices << [column_index, row_index]
         end
       end
     end
   end
 
-  def blur
-    one_pixel_indices = self.one_coordinates
-    @new_image = Marshal.load( Marshal.dump(@rows) )    
-    one_pixel_indices.each do |row, column|
-      @new_image[row][column - 1] = 1 if column != 0
-      @new_image[row - 1][column] = 1 if column != @new_image[0].length - 1
-      @new_image[row + 1][column] = 1 if row != 0
-      @new_image[row][column + 1] = 1 if row != @new_image.length - 1
-    end
-    @new_image.each{|new| puts new.join}
+  def blur_above(x,y)
+    @rows[y-1][x] = 1 unless on_upper_edge?(y)
   end
-end  
+
+  def blur_below(x,y)
+    @rows[y+1][x] = 1 unless on_bottom_edge?(y)
+  end
+
+  def blur_left(x,y)
+    @rows[y][x-1] = 1 unless on_left_edge?(x)
+  end
+
+  def blur_right(x,y)
+    @rows[y][x+1] = 1 unless on_right_edge?(x)
+  end
+
+  def on_left_edge?(x)
+    x == 0
+  end
+
+  def on_right_edge?(x)
+    x == @rows.first.size - 1
+  end
+
+  def on_upper_edge?(y)
+    y == 0
+  end
+
+  def on_bottom_edge?(y)
+    y == @rows.size - 1
+  end
+
+  def blur
+    @one_pixel_indices.each do |row, column|
+      blur_left(row, column)
+
+      blur_right(row, column)
+
+      blur_above(row, column)
+
+      blur_below(row, column)
+    end
+  end
+end
 
 image = Image.new([
   [0, 0, 0, 0],
   [0, 1, 0, 0],
   [0, 0, 0, 1],
   [0, 0, 0, 0]
-  ])
+])
 
-image.output_image
-puts "\n"
 image.blur
+image.output_image
