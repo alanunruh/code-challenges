586eb6f5ce03f0fa46fc8443b52ab6999d2daba6
Alan Unruh <alan.unruh@gmail.com>
2017-06-18 13:42:48 +0000

    const keyword example

---
+++ javascript/es6/const.js
@@ -0,0 +1,7 @@
+// Presented with an error message that indicates Assingment to constant variable.
+ // You will generally use a const if you can. In situations where the value needs to change, you'll generally use let.
+
+"use strict";
+
+const value = "hello";
+value = "goodbye";
