5c28925c0e1ad9f72a6f68c65f672014ec7cf1c2
Alan Unruh <alan.unruh@gmail.com>
2017-06-18 13:15:18 +0000

    Strict mode

---
+++ javascript/es6/strict.js
@@ -0,0 +1,6 @@
+// If the first line of a program is the string use strict, the NodeJS knows to use strict mode.
+// This means if you're using an aspect of the JavaScript language that's generally frowned upon it will present you an error.
+
+"use strict";
+
+console.log("I'm using strict mode");
