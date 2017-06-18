04860820153f777a3f6c26b96857f8a9e8f16090
Alan Unruh <alan.unruh@gmail.com>
2017-06-18 13:45:58 +0000

    Iteration examplw

---
+++ javascript/es6/iteration.js
@@ -0,0 +1,14 @@
+// ES6 supports iterating similar to Ruby.
+// This will loop through each of the items in the array and display the value in the console.
+
+"use strict";
+
+const values = [
+  "hello",
+  "goodbye",
+  "good day"
+]
+
+for(let value of values) {
+  console.log(value)
+}
